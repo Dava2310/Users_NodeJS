33b24edcbbe289936da2a75f71939fe4408cffe7
Dava2310 <dvetecourt23@gmail.com>
2024-07-14 11:37:39 -0400

    Endpoints of Getting all Users, Updating and Deleting + Validations on duplicates of email and username in API and View.

--- src/modules/users/controller.js
+++ src/modules/users/controller.js
@@ -1,5 +1,6 @@
 import pool from '../../DB/mysql.js';
 import {compare} from 'bcrypt'
+
 /**
  * Validates if a username already exists in the database.
  * @param {string} username - The username to check.
@@ -20,6 +21,25 @@ const validateUsername = async (username) => {
 };
 
 /**
+ * Validates if a username already exists in the database but not in this user.
+ * @param {string} username - The username to check.
+ * @returns {Promise<boolean>} - Returns true if the username exists, false otherwise.
+ */
+const validateUsernameNotId = async (username, id) => {
+    try {
+        const connection = await pool.getConnection();
+        const [rows] = await connection.query('SELECT * FROM users WHERE username = ? AND id != ?', [username, id]);
+        connection.release();
+
+        // Check if the query returned any results
+        return rows.length > 0;
+    } catch (error) {
+        console.error('Error validating username:', error);
+        throw error;
+    }
+};
+
+/**
  * Validates if an email already exists in the users table.
  * @param {string} email - The username to check.
  * @returns {Promise<boolean>} - Returns true if the email exists, false otherwise.
@@ -28,6 +48,25 @@ const validateEmail = async (email) => {
     try {
         const connection = await pool.getConnection();
         const [rows] = await connection.query('SELECT * FROM users WHERE email = ?', [email]);
+        connection.release();
+
+        // Check if the query returned any results
+        return rows.length > 0;
+    } catch (error) {
+        console.error('Error validating email:', error);
+        throw error;
+    }
+};
+
+/**
+ * Validates if an email already exists in the users table but not in this user.
+ * @param {string} email - The username to check.
+ * @returns {Promise<boolean>} - Returns true if the email exists, false otherwise.
+ */
+const validateEmailNotId = async (email, id) => {
+    try {
+        const connection = await pool.getConnection();
+        const [rows] = await connection.query('SELECT * FROM users WHERE email = ? AND id != ?', [email, id]);
         connection.release();
 
         // Check if the query returned any results
@@ -94,10 +133,33 @@ const getUserById = async (id) => {
     }
 };
 
+const getUsersPaginated = async (page, count) => {
+    try {
+        const offset = (page - 1) * count;  // Calcular el offset
+
+        const [rows] = await pool.query('SELECT id, name, last_name, username, email FROM users LIMIT ? OFFSET ?', [count, offset]);
+
+        const [totalRows] = await pool.query('SELECT COUNT(*) as total FROM users');
+
+        return {
+            users: rows,
+            total: totalRows[0].total,
+            page,
+            count
+        };
+    } catch (err) {
+        console.error('Error fetching users:', err);
+        throw err;
+    }
+};
+
 export default {
-    validateUsername, 
+    validateUsername,
+    validateUsernameNotId, 
     validateEmail, 
+    validateEmailNotId,
     authenticateUser,
     getData,
-    getUserById
+    getUserById,
+    getUsersPaginated
 };
--- src/modules/users/routes.js
+++ src/modules/users/routes.js
@@ -3,7 +3,16 @@ import {hashSync} from 'bcrypt'
 import pool from '../../DB/mysql.js'
 import controllers from './controller.js';
 
-const {validateUsername, validateEmail, authenticateUser, getData, getUserById} = controllers;
+const {
+    validateUsername, 
+    validateEmail, 
+    authenticateUser, 
+    getData, 
+    getUserById, 
+    getUsersPaginated,
+    validateUsernameNotId,
+    validateEmailNotId
+} = controllers;
 
 const router = Router();
 
@@ -92,7 +101,10 @@ router.get('/dashboard', async(req, res) => {
 
     // Verificar si hay un mensaje en la sesión
     const message = req.session.message;
+    const alert_message = req.session.alert_message;
+
     delete req.session.message; // Limpiar el mensaje después de leerlo
+    delete req.session.alert_message;
 
     if (!req.session.userId) {
         res.redirect('/login'); // Redirige si no hay sesión activa
@@ -102,22 +114,47 @@ router.get('/dashboard', async(req, res) => {
     const user = await getData(req.session.userId);
     console.log(user);
 
-    res.render('users/dashboard', {user: user, message, layout: 'system'});
+    res.render('users/dashboard', {user: user, message, alert_message, layout: 'system'});
 })
 
 // UPDATE
 router.post('/update', async(req, res) => {
     try {
-        const {name, last_name, email} = req.body;
+        const {name, last_name, username, email} = req.body;
         const id = req.session.userId;
         
-        await pool.query('UPDATE users SET name =?, last_name =?, email =? WHERE id =?', [name, last_name, email, id]);
+        // Check if the user exists
+        const [rows] = await pool.query('SELECT * FROM users WHERE id = ?', [id]);
+        if (rows.length === 0) {
+            res.redirect('/logout');
+        }
+
+        // Validate if username already exists
+        const usernameExists = await validateUsernameNotId(username, id);
+
+        if (usernameExists) {
+            // Handle the case where the username already exists
+            req.session.alert_message = 'Username not valid or already in use in other users'
+            res.redirect('/dashboard');
+            return;
+        }
+
+        // Validate if email already exists
+        const emailExists = await validateEmailNotId(email, id);
+
+        if (emailExists) {
+            // Handle the case where the username already exists
+            req.session.alert_message = 'Email not valid or already in use in other users';
+            res.redirect('/dashboard');
+            return;
+        }
+
+        await pool.query('UPDATE users SET name =?, last_name =?, email =?, username=? WHERE id =?', [name, last_name, email, username, id]);
         
         // Guardar mensaje en la sesión
         req.session.message = 'User updated successfully';
-
         res.redirect('/dashboard');
-        
+        return;
         
     } catch (err) {
         console.error('Error al actualizar el usuario:', err);
@@ -159,5 +196,83 @@ router.get('/api/v1/users/:id', async(req, res) => {
         res.status(500).json({ message: 'Error fetching user' });
     }
 });
+
+// Listing all users
+router.get('/api/v1/users', async (req, res) => {
+    try {
+        const page = parseInt(req.query.page) || 1;  // Página actual
+        const count = parseInt(req.query.count) || 10;  // Cantidad de usuarios por página
+
+        const users = await getUsersPaginated(page, count);
+
+        res.json(users);
+    } catch (err) {
+        console.error('Error fetching users:', err);
+        res.status(500).json({ message: 'Error fetching users' });
+    }
+});
+
+// UPDATING ONE USER
+router.put('/api/v1/users/:id', async(req, res) => {
+    try {
+        
+        // Getting the ID
+        const id = parseInt(req.params.id);
+
+        // Check if the user exists
+        const [rows] = await pool.query('SELECT * FROM users WHERE id = ?', [id]);
+        if (rows.length === 0) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        // Getting the other data
+        const {name, last_name, username, email} = req.body;
+
+        // Validate if username already exists
+        const usernameExists = await validateUsernameNotId(username, id);
+
+        if (usernameExists) {
+            // Handle the case where the username already exists
+            return res.status(403).json({ message: 'Username not valid or already in use in other users' });
+        }
+
+        // Validate if email already exists
+        const emailExists = await validateEmailNotId(email, id);
+
+        if (emailExists) {
+            // Handle the case where the username already exists
+            return res.status(403).json({ message: 'Email not valid or already in use in other users' });
+        }
+
+        // Inserting that into the SQL Query
+        await pool.query('UPDATE users SET name =?, last_name =?, email =?, username =? WHERE id =?', [name, last_name, email, username, id]);
+
+        const message = "User updated successfully";
+        res.status(200).json({ message });
+
+    } catch (err) {
+        console.error('Error fetching users:', err);
+        res.status(500).json({ message: 'Error updating user' });
+    }
+});
+
+// DELETING ONE USER
+router.delete('/api/v1/users/:id', async(req, res) => {
+    try {
+        const id = parseInt(req.params.id);
+        const [rows] = await pool.query('SELECT * FROM users WHERE id =?', [id]);
+        
+        if (rows.length === 0) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        
+        await pool.query('DELETE FROM users WHERE id =?', [id]);
+        res.status(200).json({ message: 'User deleted successfully' });
+        
+    } catch (err) {
+        console.error('Error fetching users:', err);
+        res.status(500).json({ message: 'Error deleting user' });
+    }
+})
 
 export default router;
